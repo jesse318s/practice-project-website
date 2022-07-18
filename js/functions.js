972dc5c5da241899fadf25841d061e4f6b784881
Jesse Sites <jesse318s@gmail.com>
2022-07-18 13:19:24 -0500

    improved JavaScript clarity, and added an event parameter to validateForm

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,43 @@
-/* form validation */
-document.getElementById('myForm').addEventListener('submit', validateForm);
+// accordion
+let acc = document.getElementsByClassName("accordion");
 
-function validateForm() {
-  let x = document.forms["myForm"]["name"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
-
-/* accordion */
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
+// accordion functionality
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
+    // Toggle between hiding and showing the active panel
     var panel = this.nextElementSibling;
+
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
       panel.style.display = "block";
     }
   });
+}
+
+
+
+// form validation
+document.getElementById('myForm').addEventListener('submit', validateForm);
+
+function validateForm(event) {
+  // set name equal to name input value in form
+  let name = document.forms["myForm"]["name"].value;
+
+  if (name === "") {
+    // prevent form refresh by default
+    event.preventDefault();
+
+    console.log("Name must be filled out.")
+
+    alert("Name must be filled out.");
+
+    return false;
+  } else {
+    return true;
+  }
 }
