0b17465ee52ea010f2d7c77a0ff0e01aba952d35
jesse318s <jesse318s@gmail.com>
2024-06-21 19:01:50 -0500

    clean code

--- js/javascript.js
+++ js/javascript.js
@@ -1,46 +1,36 @@
-// accordion
 let acc = document.getElementsByClassName("accordion");
 
-// accordion functionality
+// Accordion functionality
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    // Toggle between hiding and showing the active panel
     let panel = this.nextElementSibling;
+
+    this.classList.toggle("active");
 
     if (panel.style.display === "block") {
       panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
+
+      return;
     }
+
+    panel.style.display = "block";
   });
 }
 
-// form validation
-document.getElementById("myForm").addEventListener("submit", validateForm);
-
-export default function validateForm(event) {
-  // set name equal to name input value in form
+// Form validation
+function validateForm(event) {
   let name = document.forms["myForm"]["name"].value;
 
-  // remove unnecessary whitespace from name input value
   name = name.trim();
 
   if (name === "") {
-    // prevent form submission and page refresh
     event.preventDefault();
-
-    // log failed name input validation
-    console.log("Name must be filled out.");
-
-    // alert user of failed name input validation
     alert("Name must be filled out.");
 
     return false;
-  } else {
-    return true;
   }
+
+  return true;
 }
+
+document.getElementById("myForm").addEventListener("submit", validateForm);
